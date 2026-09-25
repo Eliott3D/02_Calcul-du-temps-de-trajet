b46b9ffa982c0926faa1b40d61a4978115462e73
EliottD <112376348+ederiazl@users.noreply.github.com>
2026-09-25 14:08:33 +0200

    fix code
    remove bonus

--- main.cpp
+++ main.cpp
@@ -11,38 +11,15 @@ Remarque(s) :
 
 int main()
 {
-    //distances are in km
+    //distances in km
     const int dx = 3;
     const int dy = 10;
-    //speeds are in km/h
+    int l1 = 6;
+    //speeds in km/h
     const int s1 = 5;
     const int s2 = 2;
 
-    int l1 = 0;
-
-    bool valid = false;
-    do
-    {
-        std::cout << "Quelle distance en km parcourt le robot sur la route ? : ";
-        std::cin >> l1;
-
-        if (std::cin.good())
-        {
-            valid = l1 > 0;
-            if (!valid)
-            {
-                std::cout << "La valeur entrée doit être positive." << std::endl;
-            }
-        }
-        else
-        {
-            std::cout << "La valeur doit être un entier positif non nul." << std::endl;
-            std::cin.clear();
-            std::cin.ignore();
-        }
-    }
-    while (!valid);
-
+    //calculating distance on rocks
     double l2 = std::sqrt(std::pow(dx, 2) + std::pow(dy-l1, 2));
     double t1 = static_cast<double>(l1)/s1;
     double t2 = l2 / s2;
